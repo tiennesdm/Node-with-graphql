ec4c88830b9bdfc671257a395e42e59cdbd3ace1
tiennesdm <shubhammehta.dronstudy@gmail.com>
2019-05-29 00:22:04 +0530

    graphql create user

---
+++ graphql/schema.js
@@ -0,0 +1,37 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+      
+    }
+
+    type AuthData {
+        token: String!
+        userId: String!
+    }
+
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootQuery {
+        login(email: String!, password: String!): AuthData!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
